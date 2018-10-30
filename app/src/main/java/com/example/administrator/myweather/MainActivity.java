51e5f66327a2cc0c6ab5e55d52e5416f2dfd0073
liu <542659321@qq.com>
2018-10-30 15:13:17 +0800

    完成省市县3级列表功能

--- app/src/main/java/com/example/administrator/myweather/MainActivity.java
+++ app/src/main/java/com/example/administrator/myweather/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.administrator.myweather;
 
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends ActionBarActivity {
-
+//public class MainActivity extends ActionBarActivity {
+public class MainActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
